b89374f11b18bda9a50f9c0dc083ba0b64b55617
Igor Avdonin <i.avdonin@okko.tv>
2024-07-29 20:01:03 +0300

    Do better

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Привет, Яндекс Практикум!!!");
-        System.out.println("Привет, Яндекс Практикум!!!");
-        System.out.println("Привет, Яндекс Практикум!!!");
-
-        System.out.println("Привет, Яндекс Практикум!!!");
-        System.out.println("Привет, Яндекс Практикум!!!");
-        System.out.println("Привет, Яндекс Практикум!!!");
     }
 }
