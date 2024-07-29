7be07662079cde426925a184f30e2be3b5a8b820
Igor Avdonin <i.avdonin@okko.tv>
2024-07-29 19:46:56 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет, Яндекс Практикум!");
-
+        System.out.println("Привет, Яндекс Практикум!!!");
     }
 }
